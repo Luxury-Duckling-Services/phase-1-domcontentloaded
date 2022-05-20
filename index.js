08e910ae11e2e62de21ac39c940adfaf2904683c
Yifan <datasciencewyfan@gmail.com>
2022-05-20 10:26:42 -0700

    commit tree b10a8220e599e08cfea5a362396def8757987330

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+// document.addEventListener('DOMContentLoaded' , ()=>{
+//     let text = document.querySelector('#text')
+//     text.innerHTML = 'This is really cool!'
+// })
+
+// document.addEventListener('DOMContentLoaded' , ()=>{
+//     let text = document.getElementById('text')
+//     text.textContent = 'This is really cool!'
+// })
+
+document.addEventListener('DOMContentLoaded' , ()=>{
+    let text = document.querySelector('#text')
+    text.textContent = 'This is really cool!'
+})
